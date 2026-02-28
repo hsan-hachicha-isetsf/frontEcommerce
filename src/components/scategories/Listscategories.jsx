35c276c3491a68dc1c08e633823df3e387a9688d
Hachicha Hassan <hsan.isetsf@gmail.com>
2026-02-28 09:28:24 +0100

    sous catégories

--- src/components/scategories/Listscategories.jsx
+++ src/components/scategories/Listscategories.jsx
@@ -1,8 +1,46 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const Listscategories = () => {
+  const[scat,setScat] = useState([])
+  const getScategories=async()=>{
+    try {
+      const res=await axios.get("http://localhost:3000/api/scategories")
+      setScat(res.data) 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    getScategories()
+  },[])
   return (
     <div>
-      Liste des sous-catégories
+      <h1>Liste des sous-catégories</h1>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Nom sous-catégorie</th>
+            <th>Image </th>
+            <th>Catégorie</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            scat.map((sc,index)=>
+              <tr key={index}>
+                <td>{sc.nomscategorie}</td>
+                <td><img src={sc.imagescategorie} alt={sc.nomscategorie} width="100"/></td>
+                <td>{sc.categorieID.nomcategorie}</td>
+                <td><button className="btn btn-warning btn-sm">Update</button></td>
+                <td><button className="btn btn-danger btn-sm">Delete</button></td>
+              </tr>  
+            )
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
